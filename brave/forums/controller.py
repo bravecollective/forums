4033a6fcde2da4b736d8db20bb160b73b4eaa050
Alice Bevan-McGregor <alice@gothcandy.com>
2014-01-10 13:19:35 -0500

    Initial code import.

---
+++ brave/forums/controller.py
@@ -0,0 +1,132 @@
+# encoding: utf-8
+
+from __future__ import unicode_literals
+
+from binascii import hexlify, unhexlify
+from hashlib import sha256
+from web.auth import authenticate, user
+from web.core import config, Controller, url, request
+from web.core.http import HTTPFound, HTTPNotFound
+from web.auth import authenticated
+from marrow.mailer import Mailer
+from ecdsa.keys import SigningKey, VerifyingKey
+from ecdsa.curves import NIST256p
+
+from brave.core.api.client import API
+from brave.forums import util
+from brave.forums.model import Forum, Thread, Comment
+
+
+log = __import__('logging').getLogger(__name__)
+
+
+class ThreadController(Controller):
+    def __init__(self, forum, id):
+        self.forum = forum
+        Thread.objects(id=id).update_one(inc__stat__views=1)
+        self.thread = Thread.objects.get(id=id)
+        super(ThreadController, self).__init__()
+    
+    def index(self, page=1, message=None, upload=None, vote=None):
+        if request.method == 'POST':
+            if not message or not message.strip():
+                return 'json:', dict(success=False, message="Empty message.")
+            
+            self.thread.stat.comments += 1
+            self.thread.comments.append(Comment(
+                    message = message,
+                    creator = user._current_obj(),
+                ))
+            self.thread.save()
+            
+            return 'json:', dict(success=True)
+        
+        return 'brave.forums.template.thread', dict(page=1, forum=self.forum, thread=self.thread)
+    
+    def __default__(self, page):
+        return 'brave.forums.template.thread', dict(page=int(page), forum=self.forum, thread=self.thread), dict(only='comments')
+
+
+class ForumController(Controller):
+    def __init__(self, short):
+        try:
+            self.forum = Forum.objects.get(short=short)
+        except Forum.DoesNotExist:
+            raise HTTPNotFound()
+        
+        super(ForumController, self).__init__()
+    
+    def index(self, page=1, title=None, message=None, upload=None, vote=None):
+        if request.method == 'POST':
+            thread = Thread(forum=self.forum, title=title)
+            thread.comments.append(Comment(
+                    message = message,
+                    creator = user._current_obj(),
+                ))
+            thread.save()
+            return 'json:', dict(success=True)
+        
+        if request.is_xhr:
+            return 'brave.forums.template.forum', dict(page=int(page), forum=self.forum), dict(only='threads')
+        
+        return 'brave.forums.template.forum', dict(page=page, forum=self.forum)
+    
+    def __lookup__(self, thread, *args, **kw):
+        return ThreadController(self.forum, thread), args
+
+
+class RootController(Controller):
+    def __init__(self):
+        # Configure mail delivery services.
+        util.mail = Mailer(config, 'mail')
+        util.mail.start()
+        
+        # Load our keys into a usable form.
+        config['api.private'] = SigningKey.from_string(unhexlify(config['api.key']), curve=NIST256p, hashfunc=sha256)
+        config['api.public'] = config['api.private'].get_verifying_key()
+    
+    def die(self):
+        """Simply explode.  Useful to get the interactive debugger up."""
+        1/0
+    
+    def index(self):
+        if authenticated:
+            return 'brave.forums.template.index', dict()
+        
+        return 'brave.forums.template.welcome', dict()
+    
+    def authorize(self):
+        # Perform the initial API call and direct the user.
+        
+        api = API(config['api.endpoint'], config['api.identity'], config['api.private'], config['api.public'])
+        
+        success = str(url.complete('/authorized'))
+        failure = str(url.complete('/nolove'))
+        
+        result = api.core.authorize(success=success, failure=failure)
+        
+        raise HTTPFound(location=result.location)
+    
+    def authorized(self, token):
+        # Capture the returned token and use it to look up the user details.
+        # If we don't have this character, create them.
+        # Store the token against this user account.
+        # Note that our own 'sessions' may not last beyond the UTC date returned as 'expires'.
+        # (Though they can be shorter!)
+        
+        # We request an authenticated session from the server.
+        
+        user = authenticate(token)
+        
+        raise HTTPFound(location='/')
+    
+    def preview(self, content):
+        import bbcode
+        return bbcode.render_html(content)
+
+    def nolove(self, token):
+        return 'brave.forums.template.whynolove', dict()
+    
+    def __lookup__(self, forum, *args, **kw):
+        return ForumController(forum), args
+    
---
+++ brave/forums/model.py
@@ -0,0 +1,149 @@
+# encoding: utf-8
+
+from __future__ import unicode_literals
+
+from datetime import datetime
+
+from web.core import config
+from mongoengine import Document, EmbeddedDocument, StringField, DateTimeField, ReferenceField, IntField, EmbeddedDocumentField, ListField, BooleanField
+from brave.core.api.client import API
+
+
+log = __import__('logging').getLogger(__name__)
+
+
+class Entity(EmbeddedDocument):
+    meta = dict(allow_inheritance=False)
+    
+    id = IntField(db_field='i')
+    name = StringField(db_field='n')
+
+
+class Ticket(Document):
+    meta = dict(
+            collection = 'Tickets',
+            allow_inheritance = False,
+            indexes = [
+                    'character.id'
+                ],
+        )
+    
+    token = StringField(db_field='t')
+    
+    character = EmbeddedDocumentField(Entity, db_field='c', default=lambda: Entity())
+    corporation = EmbeddedDocumentField(Entity, db_field='o', default=lambda: Entity())
+    alliance = EmbeddedDocumentField(Entity, db_field='a', default=lambda: Entity())
+    
+    expires = DateTimeField(db_field='e')
+    seen = DateTimeField(db_field='s')
+    
+    @classmethod
+    def authenticate(cls, identifier, password=None):
+        """Validate the given identifier; password is ignored."""
+        
+        api = API(config['api.endpoint'], config['api.identity'], config['api.private'], config['api.public'])
+        result = api.core.info(identifier)
+        
+        user = cls.objects(character__id=result.character.id).first()
+        
+        if not user:
+            user = cls(token=identifier, expires=result.expires, seen=datetime.utcnow())
+            user.character.id = result.character.id
+            user.character.name = result.character.name
+            user.corporation.id = result.corporation.id
+            user.corporation.name = result.corporation.name
+            
+            if result.alliance:
+                user.alliance.id = result.alliance.id
+                user.alliance.name = result.alliance.name
+            
+            user.tags = result.tags if 'tags' in result else []
+            
+            user.save()
+        
+        else:
+            # TODO: Also update the corporate details, if appropriate.
+            user.update(set__token=identifier, set__seen=datetime.utcnow(), set__tags=result.tags)
+        
+        return user.id, user
+    
+    @classmethod
+    def lookup(cls, identifier):
+        """Thaw current user data based on session-stored user ID."""
+        
+        user = cls.objects(id=identifier).first()
+        
+        if user:
+            user.update(set__seen=datetime.utcnow())
+        
+        return user
+
+
+class Forum(Document):
+    meta = dict(
+            collection = 'Forums',
+            allow_inheritance = False,
+            indexes = [
+                ],
+        )
+    
+    short = StringField(db_field='s')
+    name = StringField(db_field='n')
+    summary = StringField(db_field='u')
+    
+    # The tag needed to read (view) or wrie (post to) this forum.
+    read = StringField(db_field='r')
+    write = StringField(db_field='w')
+    
+    @property
+    def threads(self):
+        return Thread.objects(forum=self)
+
+
+class Comment(EmbeddedDocument):
+    meta = dict(allow_inheritance=False)
+    
+    message = StringField(db_field='m')
+    
+    creator = ReferenceField('Ticket')
+    created = DateTimeField(db_field='dc', default=datetime.utcnow)
+    modified = DateTimeField(db_field='dm')
+    
+    upload = None  # TODO: some day we'll allow file uploads
+
+
+class ThreadFlags(EmbeddedDocument):
+    meta = dict(allow_inheritance=False)
+    
+    locked = BooleanField(db_field='l', default=False)
+    sticky = BooleanField(db_field='s', default=False)
+    hidden = BooleanField(db_field='h', default=False)
+    uploads = BooleanField(db_field='u', default=False)
+
+
+class ThreadStats(EmbeddedDocument):
+    meta = dict(allow_inheritance=False)
+    
+    comments = IntField(db_field='c', default=0)
+    uploads = IntField(db_field='u', default=0)
+    votes = IntField(db_field='v', default=0)
+    views = IntField(db_field='i', default=0)
+
+
+class Thread(Document):
+    meta = dict(
+            collection = 'Threads',
+            allow_inheritance = False,
+            indexes = [
+                ],
+            ordering = ['-id']
+        )
+    
+    forum = ReferenceField(Forum, required=True)
+    
+    title = StringField(db_field='t')
+    comments = ListField(EmbeddedDocumentField(Comment), db_field='c', default=list)
+    
+    flag = EmbeddedDocumentField(ThreadFlags, db_field='f', default=lambda: ThreadFlags())
+    stat = EmbeddedDocumentField(ThreadStats, db_field='s', default=lambda: ThreadStats())
+
