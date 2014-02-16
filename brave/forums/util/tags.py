31c78ecf7c8e3c41bbd4608f8b0f7f1bdb72a7df
noilek <github2@waughonline.com>
2014-02-16 12:46:54 -0500

    Add character, corp, and alliance tags
    Adds character, corp, and alliance tags

--- brave/forums/util/tags.py
+++ brave/forums/util/tags.py
@@ -2,6 +2,7 @@
 
 from __future__ import unicode_literals
 from bbcode import Parser
+from urllib import quote
 
 log = __import__('logging').getLogger(__name__)
 
@@ -11,6 +12,9 @@ class SemanticTagParser(object):
 		self.standalone_available_tags = {
 			'System' : self.format_dotlan,
 			'Region' : self.format_dotlan,
+			'Character' : self.format_evewho,
+			'Corporation' : self.format_evewho,
+			'Alliance'	: self.format_evewho,
 			'DebugTag' : self.format_logging
 		}
 		for tag, parser in self.standalone_available_tags.iteritems():
@@ -24,6 +28,14 @@ class SemanticTagParser(object):
 
 	def format_system(self, tag_name, value, options, parent, context):
 		return '<a href="http://evemaps.dotlan.net/system/%s" target="dotlan">%s</a>' % ( options[tag_name], options[tag_name].replace(' ','_'), options[tag_name] )
+
+	def format_evewho(self, tag_name, value, options, parent, context):
+		path = {
+			'corporation' : 'corp',
+			'alliance' : 'ali',
+			'character' : 'pilot'
+		}[tag_name.lower()]
+		return '<a href="http://evewho.com/%s/%s" target="evewho">%s</a>' % ( path, quote( options[tag_name] ), options[tag_name] )
 
 	def format_logging(self, tag_name, value, options, parent, context):
 		return "<pre>tag name=%s\nvalue=%s\noptions=%s\nparent=%s\ncontext=%s</pre>" % ( tag_name, value, options, parent, context )
