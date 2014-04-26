2412d26439bc2955e05dbd8954ab63bcd759a7eb
Josiah Boning <jboning@gmail.com>
2014-04-26 03:22:07 -0700

    Ensure we don't slam osmium with repeated requests

--- brave/forums/util/osmium.py
+++ brave/forums/util/osmium.py
@@ -16,22 +16,32 @@ class Fit(Document):
                 ],
         )
     
-    eft = StringField(db_field='f', primary_key=True)
+    eft = StringField(db_field='f', primary_key=True, unique=True)
     clf = StringField(db_field='c')
     
     @staticmethod
     def get_fit(eft):
+        # We do this little dance to guarantee we make only one request to osmium for an given fit.
         query = Fit.objects(eft=eft)
         if query:
             return query[0]
-        
+        f = Fit(eft=eft)
+        try:
+            f.save()
+        except OperationError: # collision on unique field
+            return Fit.objects(eft=eft).first()
+
+        # Okay, we were the first to insert the fit into the DB. Go ahead and populate the CLF.
+
         resp = requests.post(OSMIUM_BASE+"/api/convert/eft/dna", data=dict(input=eft))
         if resp.status_code == 200:
             clf = resp.text
         else:
             clf = None
         
-        return Fit(eft=eft, clf=clf).save()
+        f.clf=clf
+        f.save()
+        return f
     
     def fit_url(self):
         if not self.clf:
