51f249e46508896ef59b8af34e7672495f6293e4
Amber Yust <amber.yust@gmail.com>
2014-07-02 12:37:40 -0700

    Non-push comments, step 4
    Also send comment ID along with comment edits, since it uses the same handler function (apparently).

--- brave/forums/component/comment/controller.py
+++ brave/forums/component/comment/controller.py
@@ -59,7 +59,7 @@ class CommentIndex(HTTPMethod):
             return 'json:', dict(success = False, message = "Thread not found.")
         
         self.thread.channel.send('refresh', str(self.comment.id))
-        return 'json:', dict(success = True)
+        return 'json:', dict(success=True, comment=self.comment.id)
     
     def delete(self):
         """Delete the comment."""
